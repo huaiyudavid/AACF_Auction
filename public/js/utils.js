370b2a2bbfc5e43e6e5385f55e6646fbee7cbef1
huaiyudavid <davidyangrocs@gmail.com>
2016-03-31 12:49:04 -0500

    debugging

--- public/js/utils.js
+++ public/js/utils.js
@@ -3,5 +3,6 @@ var website = 'http://vandyaacf.tech/auction/';
 function parseResponse(response)
 {
 	var resString = response.substr(8);
+    console.log(resString);
 	return JSON && JSON.parse(resString) || $.parseJSON(resString);
 }
